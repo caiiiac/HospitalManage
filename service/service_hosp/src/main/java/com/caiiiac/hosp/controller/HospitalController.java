987c39e52e3252a4a0c25ae1de1c12879440eeaf
caijq <caiiiac@163.com>
2021-06-04 15:14:21 +0800

    添加 nacos

---
+++ service/service_hosp/src/main/java/com/caiiiac/hosp/controller/HospitalController.java
@@ -0,0 +1,11 @@
+package com.caiiiac.hosp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin/hosp/hospital")
+@CrossOrigin
+public class HospitalController {
+}
